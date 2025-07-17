293a6ac5b4e881d9777d12978f7e3d3ef7cf46b7
Vladyslav Synytskyi <vladyslavsynytskyi1019@gmail.com>
2025-07-17 11:42:26 +0300

    Delete static directory

--- static/js/script.js
+++
@@ -1,6 +0,0 @@
-// Disable video on small screens
-if (window.innerWidth < 768) {
-  const vid = document.getElementById('bg-video');
-  vid.parentNode.removeChild(vid);
-  document.body.style.background = 'url(media/videos/fallback.jpg) center/cover no-repeat';
-}
